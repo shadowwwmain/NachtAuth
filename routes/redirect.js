a0219e96db346f87bfae34937f518cdbfacb99c2
Gute Nacht <93240515+gutenacht0221@users.noreply.github.com>
2023-02-02 01:43:58 -0500

    Delete routes directory

--- routes/redirect.js
+++
@@ -1,5 +0,0 @@
-const config = require('../../config.json');
-
-module.exports = async(req, res) => {
-    res.redirect(`https://login.live.com/oauth20_authorize.srf?client_id=${config.azure.client_id}&response_type=code&redirect_uri=${config.azure.redirect_uri}&scope=XboxLive.signin+offline_access&state=${req.query.callback}`);
-}
